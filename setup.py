72c87ee757f30dc4fd74a77994dc60a159ed0683
Sleft <fte08eas@student.lu.se>
2013-04-01 20:00:13 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+setup(name='cliask',
+      version='0.0.1',
+      py_modules=['cliask'],
+      description = "A module for getting validated user input via the console",
+      author = "Sleft",
+      author_email = "fte08eas@student.lu.se",
+      url = "https://github.com/Sleft/cliask",
+      classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "Environment :: Console",
+        "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: User Interfaces",
+        "Topic :: System :: Shells",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        )
